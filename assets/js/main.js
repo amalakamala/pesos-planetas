5a92c31d9581a00a799dd1a07c91fa8ede24a265
Paula Ponce <paula.poncez@usach.cl>
2017-05-08 20:46:19 -0300

    calculo de peso para saturno y urano en main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function elPeso(){
+	var elPeso = document.getElementById('peso').value;
+	var planeta = document.getElementById('elegir-planeta').value;
+
+	if(planeta == "saturno"){
+		alert("Tu peso en Saturno es de: " + ((parseInt(elPeso) * 10.44) / 9.8).toFixed(3) + " Kg");
+	}else if(planeta == "urano"){
+		alert("Tu peso en Urano es de: " + ((parseInt(elPeso) * 8.69) / 9.8).toFixed(3) + " Kg");
+	}
+}
