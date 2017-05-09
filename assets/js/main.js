3945184405f90b15e1003b3ba1c188c1264c52cc
amalakamala <amalakamala.a@gmail.com>
2017-05-08 22:20:58 -0300

    arreglo detalles js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function elPeso(){
 		alert("Tu peso en Mercurio es de: " + ((parseInt(elPeso) * 3.7) / 9.8).toFixed(3) + "kg");
 	}
 	else if(planeta == "venus"){
-		alert("Tu peso en Mercurio es de: " + ((parseInt(elPeso) * 8.87) / 9.8).toFixed(3));
+		alert("Tu peso en Mercurio es de: " + ((parseInt(elPeso) * 8.87) / 9.8).toFixed(3) + " Kg");
 	}
 	else if(planeta == "saturno"){
 		alert("Tu peso en Saturno es de: " + ((parseInt(elPeso) * 10.44) / 9.8).toFixed(3) + " Kg");
